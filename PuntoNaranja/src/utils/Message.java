616e97175e72ef9181e3e742a673255c61e135f5
RonaldLobo <RonaldLobo>
2015-02-16 09:29:22 -0600

    cambio en message

--- PuntoNaranja/src/utils/Message.java
+++ PuntoNaranja/src/utils/Message.java
@@ -326,14 +326,6 @@ public class Message {
     static String sha1(String input) throws NoSuchAlgorithmException {
         String sh = "";
         try {
-            //        MessageDigest mDigest = MessageDigest.getInstance("SHA1");
-//        byte[] result = mDigest.digest(input.getBytes());
-//        StringBuffer sb = new StringBuffer();
-//        for (int i = 0; i < result.length; i++) {
-//            sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
-//        }
-//         
-//        return sb.toString();
             sh = AeSimpleSHA1.SHA1(input);  
         } catch (UnsupportedEncodingException ex) {
             Logger.getLogger(Message.class.getName()).log(Level.SEVERE, null, ex);
